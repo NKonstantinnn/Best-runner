e024b1317bd8070e8a886d4f901151565c89bea6
kostya007 <kostya.1998.nosarev@mail.ru>
2019-12-27 21:46:26 +0700

    Fix a redux-form select component

--- client/src/containers/JournalContainer/containers/TrainingModal.jsx
+++ client/src/containers/JournalContainer/containers/TrainingModal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { Button, Input } from 'reactstrap';
+import { Button, Input, FormGroup, Label } from 'reactstrap';
 import moment, { Moment } from 'moment';
 
-import Select from '../../../shared/redux-form-components/CustomSelect';
+import Select from '../../../shared/redux-form-components/Select';
 import DatePicker from '../../../shared/redux-form-components/DatePicker';
 import Slider from '../../../shared/redux-form-components/CustomSlider';
 import CustomField from '../../../shared/redux-form-components/CustomField';
@@ -27,15 +27,22 @@ const TrainingModal = (props) => {
     <TrainingModalContent>
       <TrainingModalTitle>{actionName} training</TrainingModalTitle>
       <TrainingModalForm onSubmit={handleSubmit}>
-        <Field name="activity" component={Select} label="Activity" options={ActivityOptions} />
-        <Field
-          name="date"
-          component={DatePicker}
-          minDate={minDate}
-          maxDate={moment()}
-          singleDatePicker
-          opens="right"
-        />
+        <FormGroup>
+          <Label>Activity</Label>
+          <Field name="activity" component={Select} options={ActivityOptions} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Date</Label>
+          <Field
+            name="date"
+            component={DatePicker}
+            minDate={minDate}
+            maxDate={moment()}
+            singleDatePicker
+            opens="right"
+            containerStyles={{ display: 'block' }}
+          />
+        </FormGroup>
         <Field name="distance" component={Slider} label="Distance" min={0.01} max={100} step={0.01} />
         <CustomField name="comment" id="comment" component={Input} label="Comment" type="textarea" />
         <TrainingModalFooter>
